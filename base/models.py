4e30bf87a155210fcacedcfd64b60729d37a1b4d
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-11-06 19:30:10 +0530

    register ready

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
 # Create your models here.
+
+choices = (
+    ('normal_user', 'normal_user'),
+    ('community', 'community'),
+    ('hotel', 'hotel'),
+)
+
+
+class base_models(models.Model):
+    '''model which is connected with User model, contain data about the user type.'''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=15, choices=choices, null=True)
+    address = models.CharField(max_length=100, null=True)
+    hotel_license = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.user.username
--- base/views.py
+++ base/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from base.models import base_models
 
 # Create your views here.
 
@@ -6,3 +9,37 @@ from django.shortcuts import render
 def login(request):
     '''funct. for selecting user type'''
     return render(request, 'User_select_login.html')
+
+
+def register(request):
+    '''function for registering new user to app.'''
+    if request.method == 'POST':
+        # get form data
+        username = request.POST['first_name']
+        email=request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        user_type = request.POST['user_type']
+        address = request.POST['address']
+
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken')
+                return redirect('register')
+            else:
+                # create user and profile objects
+                user = User.objects.create_user(
+                    username=username, password=password, email=email)
+                user.save()
+                profile = base_models.objects.create(
+                    user=user, user_type = user_type, address = address)
+                profile.save()
+                messages.success(request, 'Account created successfully.')
+                return render(request, 'User_select_login.html')
+
+        else:
+            messages.info(request, 'Both passwords are not matching')
+            return redirect('register')
+
+    return render(request, 'register.html')
