05b9ceb4091c23423cfd6644a113e23b9f9b129b
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-11-06 19:55:41 +0530

    normal_user login sett

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('login',views.user_login,name="user_login"),
+    path('user_login',views.user_login,name="user_login"),
+    path('user_logout',views.user_logout,name="user_logout"),
+    path('user_home', views.user_home, name="user_home"),
 ]
--- users/views.py
+++ users/views.py
@@ -4,27 +4,42 @@ from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from base.models import base_models
 
 # Create your views here.
 
 
 def user_login(request):
-    '''funct for normal user login'''
+    '''function for normal user login'''
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
         user = authenticate(
             request=request, username=username, password=password)
-        if user is not None:
-            if user.is_superuser:
-                messages.error(request, "Error in login")
-                return render(request, 'user_login.html')
-            else:
-                login(request=request, user=user)
-                #return HttpResponseRedirect(reverse("base"))
-                return
+
+        if user is not None and not user.is_superuser:
+            # Check the user's user_type from the base_models model
+            try:
+                user_type = base_models.objects.get(user=user).user_type
+                if user_type == 'normal_user':
+                    login(request, user)
+                    # Redirect to the desired page upon successful login
+                    return HttpResponseRedirect(reverse("user_home"))
+                else:
+                    messages.error(request, "You are not allowed to log in.")
+            except base_models.DoesNotExist:
+                messages.error(request, "User type information not found.")
         else:
-            messages.error(request, "Error in login")
-            return render(request, 'user_login.html')
+            messages.error(request, "Username or Password doesn't exist!!")
+
+        return render(request, 'user_login.html')
     else:
         return render(request, 'user_login.html')
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "Logout Successfull!")
+    return render(request, 'user_login.html')
+
+def user_home(request):
+    return render(request, 'user_home.html')
