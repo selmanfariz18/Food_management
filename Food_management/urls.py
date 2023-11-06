89374ebab7745ad6f53a680da22c31147c6173b3
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-11-06 18:49:35 +0530

    home page ui

--- Food_management/urls.py
+++ Food_management/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf.urls.static import static
+from Food_management import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('base.urls')), #urls routed to base app
     path('users/',include('users.urls')), #urls routed to users app
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
