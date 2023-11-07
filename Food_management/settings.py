b338c2bf7476b4f879595dac96f8dafdbb2ae630
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-11-07 22:28:46 +0530

    sett changes

--- Food_management/settings.py
+++ Food_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8=7&qm@eh0e%t6qvbkrs2&swk4@%ph36^7q3$7(jb0d&b*tfb_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
